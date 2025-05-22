516c507601ddf548970803292d503c28dbae8edb
Pedro Vinicios <110180491+PedroVinicioss@users.noreply.github.com>
2025-05-22 14:38:15 -0300

    Solução do Desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+//var smartphone  = new Smartphone();
+var nokia = new Nokia("11997011131", "NOK001", "JKJ12MS", 16);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Facebook");
+
+var iphone = new Iphone("22997022242", "APPLE001", "MLM34NT", 32);
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
